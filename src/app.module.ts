7b041a43333f8041b32cc15a9d2471aea702ab48
Juan Pablo Mitriatti <juan.mitriatti@gmail.com>
2023-03-13 23:17:10 -0300

    Adjuntando certificados para el protocolo ssl.

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { DatabaseModule } from './database/database.module';
 import { FacturaModule } from './modules/factura/factura.module';
 import { LineaModule } from './modules/linea/linea.module';
 import { ProductoModule } from './modules/producto/producto.module';
+import { ConfigModule } from '@nestjs/config';
 
  @Module({
-  imports: [DatabaseModule, ProductoModule, FacturaModule, LineaModule],
+  imports: [DatabaseModule, ProductoModule, FacturaModule, LineaModule, ConfigModule.forRoot()],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
+
+// definimos la ruta
+const crPath = '/etc/letsencrypt/live/api.letrix.xyz/fullchain.pem';
+const pkPath = '/etc/letsencrypt/live/api.letrix.xyz/privkey.pem';
+const options: any = {};
+
+const environment = process.env.environment || 'development';
+if (environment != 'development') {
+  // validamos si los archivos existen
+  if (fs.existsSync(crPath) && fs.existsSync(pkPath)) {
+    // cargamos los archivos sobre las options
+    options.httpsOptions = {
+      cert: fs.readFileSync(crPath),
+      key: fs.readFileSync(pkPath)
+    }
+  }
+}
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, options);
   app.enableCors();
-
   await app.listen(3000);
 }
 bootstrap();
